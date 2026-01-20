fad0ab7868a85fa0ca2ee74eed85c95ef6bef4a0
Alessio Tucci <alessiotucci33@gmail.com>
2026-01-20 14:02:40 +0100

    now it works, created some user, thanks riky

---
+++ api/register.php
@@ -0,0 +1,51 @@
+<?php
+//1) headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Contet-Type: application/json');
+
+
+include_once '../config/db_params.php';
+include_once '../api/DbConnection.php';
+include_once '../models/User.php';
+
+
+$database = new DbConnection();
+$db = $database->connect();
+$user = new User($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->email) && !empty($data->password) && !empty($data->role))
+{
+		$user->email = $data->email;
+		$user->password = password_hash($data->password, PASSWORD_DEFAULT);
+		$user->role = $data->role;
+
+		if ($user->emailExists())
+		{
+			http_response_code(400);
+			echo json_decode(array("message" => "Email already in use!."));
+		}
+		else
+		{
+			if ($user->create())
+			{
+			http_response_code(201);
+			echo json_decode("User successfully created!"); //TODO now it works
+			}
+			else
+			{
+			http_response_code(503);
+			echo json_decode(array("message" => "Unable to create user."));
+			}
+		}
+
+}
+else
+{
+	http_response_code(400);
+	echo json_encode(array("message" => "Unable to create user. Data is incomplete."));
+}
+?>
--- models/User.php
+++ models/User.php
@@ -18,31 +18,42 @@ class User
 	}
 	public function create()
 	{
-	$query = 'INSERT INTO ' . $this->table . 'SET email = :email, password = :password, role = :role';
+	$query = ' INSERT INTO ' . $this->table . ' SET email = :email, password = :password, role = :role ';
+	// ALTERNATIVE (Standard SQL)
+	//$query = 'INSERT INTO ' . $this->table . ' (email, password, role) 
+    //      VALUES (:email, :password, :role)';
+
 	$stmt = $this->conn->prepare($query);
 	$this->email = htmlspecialchars(strip_tags($this->email));
 	$this->password = htmlspecialchars(strip_tags($this->password));
 	$this->role = htmlspecialchars(strip_tags($this->role));
 
+	//TODO
+	// 4. BIND PARAMETERS (You were missing this part!)
+        // This tells PDO: "Put the value of $this->email where you see :email"
+        $stmt->bindParam(':email', $this->email);
+        $stmt->bindParam(':password', $this->password);
+        $stmt->bindParam(':role', $this->role);
+
 	if ($stmt->execute())
 	{
-			printf("Success! Created the use\n");
+			printf("Success! Created the user!\n");
 			return (true);
 	}
 	else
 	{
-	printf("Error: %s.\n, $stmt->error);
+	printf("Error: %s.\n", $stmt->error);
 	return (false);
 	}
 	}
 
 	public function emailExists()
 	{
-	$query = 'SELECT id, first_name, last_name, password, role FROM ' . $this->table . 'WHERE email = ? LIMIT 0,1';
+	$query = 'SELECT *  FROM ' . $this->table . ' WHERE email = ? LIMIT 0,1';
 	$stmt = $this->conn->prepare($query);
 	$this->email = htmlspecialchars(strip_tags($this->email));
 	$stmt->bindParam(1, $this->email);
-	$stmt->execute(();
+	$stmt->execute();
 			if ($stmt->rowCount() > 0)
 			{
 			$row = $stmt->fetch(PDO::FETCH_ASSOC);
