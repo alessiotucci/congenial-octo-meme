1fc4dfdc1ab472e7301b4938a156881d3ae27076
Alessio Tucci <alessiotucci33@gmail.com>
2026-01-28 17:13:24 +0100

    pushing the part I need the most!

---
+++ api/delivery_driver/delete.php
@@ -0,0 +1,11 @@
+<?php
+/* ************************************************************************** */
+/*     File: api\delivery_driver\delete.php                                   */
+/*     Author: atucci <atucci@student.42.fr>                                  */
+/*     Created: 2026/01/28 17:12:38                                           */
+/*     Updated: 2026/01/28 17:12:49                                           */
+/*     System: unknown [SurfaceLaptopmy]                                      */
+/*     Hardware: unknown | RAM: Unknown                                       */
+/* ************************************************************************** */
+
+?>
---
+++ api/delivery_driver/update.php
@@ -0,0 +1,11 @@
+<?php
+/* ************************************************************************** */
+/*     File: api\delivery_driver\update.php                                   */
+/*     Author: atucci <atucci@student.42.fr>                                  */
+/*     Created: 2026/01/28 17:11:56                                           */
+/*     Updated: 2026/01/28 17:12:09                                           */
+/*     System: unknown [SurfaceLaptopmy]                                      */
+/*     Hardware: unknown | RAM: Unknown                                       */
+/* ************************************************************************** */
+
+?>
