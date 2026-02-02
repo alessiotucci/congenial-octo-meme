468c1c3eb0f89459524be17ca1bb0b59014b42d1
Alessio Tucci <alessiotucci33@gmail.com>
2026-02-02 12:56:50 +0100

    added a redirection in index.php, docker do not work in 42

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/* ************************************************************************** */
+/*     File: index.php                                                        */
+/*     Author: atucci <atucci@student.42.fr>                                  */
+/*     Created: 2026/02/02 12:56:15                                           */
+/*     Updated: 2026/02/02 12:56:20                                           */
+/*     System: Linux [e4r3p4.42roma.it]                                       */
+/*     Hardware: Intel Core i5-8600 | RAM: 15GB                               */
+/* ************************************************************************** */
+// src/index.php
+// Redirect user to the frontend
+header("Location: /frontend/index.html");
+exit();
+?>
